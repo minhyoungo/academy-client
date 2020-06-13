66be7c3903b06760934883930e3c968269e3b39e
Mac <mac@Macui-MacBookPro.local>
2020-06-13 15:43:49 +0900

    aa

--- src/screens/ACSC00.js
+++ src/screens/ACSC00.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
+import Maincomponent1 from "../components/MainComponent1";
+import Maincomponent2 from "../components/MainComponent2";
 
 class ACSC00 extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentTab: 1,
+    };
+  }
   render() {
+    const { currentTab } = this.state;
+
     return (
       <div className="ACSC00">
         <div className="ACSC00__bg">
@@ -15,8 +26,41 @@ class ACSC00 extends React.Component {
             <ArrowDownwardIcon />
           </div>
         </div>
+
+        <div className="ACSC00__contents">
+          <button
+            className={
+              currentTab === 1
+                ? "ACSC00__contents__btn active"
+                : "ACSC00__contents__btn"
+            }
+            onClick={() => this._changeTabHandler(1)}
+          >
+            NEWS
+          </button>
+          <button
+            className={
+              currentTab === 2
+                ? "ACSC00__contents__btn active"
+                : "ACSC00__contents__btn"
+            }
+            onClick={() => this._changeTabHandler(2)}
+          >
+            GALLERY
+          </button>
+        </div>
+        <div className="ACSC00__desc">
+          {currentTab === 1 ? <Maincomponent1 /> : null}
+          {currentTab === 2 ? <Maincomponent2 /> : null}
+        </div>
       </div>
     );
   }
+
+  _changeTabHandler = (tab) => {
+    this.setState({
+      currentTab: tab,
+    });
+  };
 }
 export default ACSC00;
